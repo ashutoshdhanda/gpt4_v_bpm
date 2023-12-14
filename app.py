88d6d5793f67b115391f87fb1abf0844f4fb71bf
Ashutosh <adandha@bitbucket.org>
2023-12-14 23:28:37 +0000

    resolve n images

--- app.py
+++ app.py
@@ -59,126 +59,73 @@ def encode_image(image_path):
 
 
 def main():
-    #load_dotenv()
     st.set_page_config(page_title="Generar resumen del imagen", page_icon=":eye:", layout="wide")
-        # Define CSS to increase text size and adjust container width
-    custom_css = """
-        <style>
-            .big-text {
-                font-size: 20px; /* Adjust text size as needed */
-            }
-            .wide-container {
-                max-width: 95%; /* You can adjust this as needed */
-            }
-        </style>
-    """
-    st.markdown(custom_css, unsafe_allow_html=True)
-    # Hide the Streamlit footer
-    hide_streamlit_style = """
-                <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
     st.write(css, unsafe_allow_html=True)
 
-    # Initialize conversation and chat history if not present
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    # Initialize a variable to hold the formatted message
     formatted_message = None
 
     with st.sidebar:
-
-        #st.image("./Deloitte_Logo.jpg", width=300)
-
         api_key = st.sidebar.text_input("Ingrese la clave Openai y presione Enter.", type="password")
-
-        uploaded_image = st.file_uploader("Subir imagen (jpg, png, bmp):", type=["jpg", "jpeg", "png", "bmp"], accept_multiple_files=True)
+        uploaded_images = st.file_uploader("Subir imágenes (jpg, png, bmp):", type=["jpg", "jpeg", "png", "bmp"], accept_multiple_files=True)
 
         if st.button("Procesar"):
-            if api_key:
+            if api_key and uploaded_images:
                 with st.spinner("Procesando..."):
-                    if uploaded_image:
-                        temp_dir = tempfile.mkdtemp()
-                        encoded_images = []  # List to store encoded images
-                        for image in uploaded_image:
-                            image_path = os.path.join(temp_dir, image.name)
-                            with open(image_path, "wb") as f:
-                                f.write(image.getvalue())
-                            base64_image = encode_image(image_path)
-                            encoded_images.append(base64_image)
-                        try:
-                            headers = {
-                                "Content-Type": "application/json",
-                                "Authorization": f"Bearer {api_key}"
-                            }
-                            payload = {
-                                "model": "gpt-4-vision-preview",
-                                "messages": [
-                                    {
-                                        "role": "system",
-                                        "content": "I have attached an image of a Business Process Model. Please analyze it in detail and provide a comprehensive description of each element and phase. This should include identifying the workflows, decision points, roles of executors, and the inputs and outputs of each activity. Also, create a table with columns for ID, Name Activity, Executor, Type of Activity, Short Description, Input, and Output, populated with data from the diagram. After that, offer recommendations for optimizing this process, focusing on aspects like redundant steps, potential bottlenecks, and opportunities for implementing automation technologies or management software to enhance process efficiency and effectiveness. Please respond in Spanish."
-                                    },
-                                    {
-                                        "role":"user",
-                                        "content": [
-                                            {
-                                                "type": "text",
-                                                "text": "Perform a detailed analysis of the attached Business Process Model image. Describe each element and phase, noting the workflows, decision points, executor roles, and the inputs and outputs of each activity. Then, prepare a table with the following columns: ID, Name Activity, Executor, Type of Activity, Short Description, Input, Output, using information from the diagram. Provide recommendations for process optimization, highlighting redundant steps, bottlenecks, and opportunities for automation technologies or management software improvements.",
-                                                #"text": "I've uploaded a BPM diagram and need a table created with 'ID', 'Activity Name', 'Executor', 'Type of Activity', 'Short Description', 'Input', and 'Output' columns, following these rules: use the number indicated or 'N/A' for ID, match the figure's shape to the legend for 'Type of Activity', and for decisions, note 'Dependent on previous activity, XXX' in 'Short Description', and the previous activity's ID in 'Input' and 'Output'; leave fields blank if the figure isn't recognized correctly."
-                                            },
-                                            {
-                                                "type": "image_url",
-                                                "image_url": {
-                                                    "url": f"data:image/jpeg;base64,{encoded_images[0]}"
-                                                }
-                                            },
-                                            {
-                                                "type": "image_url",
-                                                "image_url": {
-                                                    "url": f"data:image/jpeg;base64,{encoded_images[1]}"
-                                                }
-                                            }
-                                        ]
-                                    }
-                                ],
-                                "max_tokens": 4096
-                            }
-                            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-                            if response.ok:
-                                response_json = response.json()
-                                messages = response_json['choices'][0]['message']['content']
-                                formatted_message = bot_template.replace("{{MSG}}", messages)
-                                filename = "saved_string.txt"
-                                with open(filename, 'w') as file:
-                                    file.write(formatted_message)
-                            else:
-                                st.error(f"Error: {response.status_code} - {response.text}")
-                        except:
-                            e = RuntimeError('Error!')
-                            st.exception(e)
-                            #print(f"Error: {response.status_code} - {response.text}")
+                    temp_dir = tempfile.mkdtemp()
+                    encoded_images = []
+                    for image in uploaded_images:
+                        image_path = os.path.join(temp_dir, image.name)
+                        with open(image_path, "wb") as f:
+                            f.write(image.getvalue())
+                        encoded_images.append(encode_image(image_path))
+
+                    messages_payload = [
+                        {
+                            "role": "system",
+                            "content": "I have attached an image of a Business Process Model. Please analyze it in detail and provide a comprehensive description of each element and phase. This should include identifying the workflows, decision points, roles of executors, and the inputs and outputs of each activity. Also, create a table with columns for ID, Name Activity, Executor, Type of Activity, Short Description, Input, and Output, populated with data from the diagram. After that, offer recommendations for optimizing this process, focusing on aspects like redundant steps, potential bottlenecks, and opportunities for implementing automation technologies or management software to enhance process efficiency and effectiveness. Please respond in Spanish."
+                        },
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type":"text",
+                                    "text":"Perform a detailed analysis of the attached Business Process Model image. Describe each element and phase, noting the workflows, decision points, executor roles, and the inputs and outputs of each activity. Then, prepare a table with the following columns: ID, Name Activity, Executor, Type of Activity, Short Description, Input, Output, using information from the diagram. Provide recommendations for process optimization, highlighting redundant steps, bottlenecks, and opportunities for automation technologies or management software improvements."
+                                }
+                            ] + [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{img}"}} for img in encoded_images]
+                            
+                        }
+                    ]
+
+                    payload = {
+                        "model": "gpt-4-vision-preview",
+                        "messages": messages_payload,
+                        "max_tokens": 4096
+                    }
+
+                    headers = {
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {api_key}"
+                    }
+
+                    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+                    if response.ok:
+                        response_json = response.json()
+                        messages = response_json['choices'][0]['message']['content']
+                        formatted_message = bot_template.replace("{{MSG}}", messages)
                     else:
-                        st.error("No subiste imagen!", icon="🚨")
+                        st.error(f"Error: {response.status_code} - {response.text}")
             else:
-                st.error("Por favor ingresar una llave valida.", icon="🚨")
+                st.error("Por favor ingresar una llave valida o subir imágenes.", icon="🚨")
 
     if formatted_message:
         st.markdown(response_css, unsafe_allow_html=True)
-        # Use a container or just st.markdown directly to display the message on the main page
-        #st.markdown('<div class="chat-container wide-container">', unsafe_allow_html=True)
-        st.markdown('<div class="response-container"><table class="response-table">', unsafe_allow_html=True)
-        #st.markdown(api_response_content)  # where `api_response_content` is the HTML content from the API response
-        st.markdown(f'<div class="big-text">{formatted_message}</div>', unsafe_allow_html=True)
-        st.markdown('</table></div>', unsafe_allow_html=True)
-    else:
-        pass
+        st.markdown(f'<div class="response-container"><table class="response-table">{formatted_message}</table></div>', unsafe_allow_html=True)
+
 
 if __name__ == '__main__':
     EULA_ACCEPTED_KEY = 'eula_accepted'
